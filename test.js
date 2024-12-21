04cd5529bbff4484d151ee23e081c9fdb8b4c02a
JamesWalsh78 <jamesdwalsh14@yahoo.com.au>
2024-12-21 13:29:03 +0000

    Update test.js

--- test.js
+++ test.js
@@ -8,3 +8,39 @@ const cards = [
 	{ name: "Larvitar", 	colour: "Brown", 	value: 1,	dfcount: 11},
 	{ name: "Lotad", 		colour: "Blue", 	value: 0,	dfcount: 2},
 ];
+
+console.log(deck);
+
+//GAME MODAL
+function setupGameModal() {
+	const modal = document.getElementById("setup-modal");
+    const form = document.getElementById("setup-form");
+	
+	const cardSelection = document.getElementById("card-selection");
+	cards.forEach(card => {
+		const optionDiv = document.createElement("div"); //creates a HTML div for each CARD
+		optionDiv.classList.add("card-option"); //creates a css element to format this Text
+		
+		optionDiv.innerHTML = `
+			<label for="${card.name}-quantity">${card.name}</label>
+			<input type="number" id="${card.name}-quantity" min="0" placeholder="${card.dfcount}">
+			`;
+			
+			cardSelection.appendChild(optionDiv); //adds the divs to the HTML
+	});
+	
+	modal.style.display = "flex";
+	
+	form.addEventListener("submit", (event) => {
+		event.preventDefault(); //stops reloading of page automatically
+		const userSelectedCards = cards.map(card => {
+			const quantityInput = document.getElementById(`${card.name}-quantity`);
+			return {
+				name: card.name,
+				count: parseInt(quantityInput.value) || card.dfcount,
+			};
+		});
+	console.log(userSelectedCards);
+	modal.style.display = "none";
+	});
+}
